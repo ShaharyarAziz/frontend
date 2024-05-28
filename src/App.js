cfd5c4e77672550e0a81b32b9fd59f087a7aa258
Shaharyar Aziz <shaharyaraziz462@gmail.com>
2024-05-28 22:02:58 +0500

    connection

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 function App() {
+  const [movies, setmovies] = useState([])
+  useEffect(() => {
+    axios.get('http://localhost:3000/movies')
+      .then((response) => {
+        setmovies(response.data); // Update the state with the response data
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.error('There was an error making the request:', error);
+      });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Full Stack Project</h1>
+        <div>{movies.length}</div>
+        {
+          movies.map((movies) => {
+            <div key={movies.id}>
+              <h1>{movies.name}</h1>
+              <p>{movies.year}</p>
+              <p>{movies.rating}</p>
+              <h1>{movies.image}</h1>
+              <h1>{movies.description}</h1>
+            </div>
+
+          })
+        }
       </header>
     </div>
   );
